5e8ecae4c1cb13b9f4ddbc427b2a89363a850b13
ashtrus <zygimantas.butas@gmail.com>
2018-12-25 00:06:28 +0100

    components updated. props added. linted. old code remoced

--- src/Dashboard/components/Categories.js
+++ src/Dashboard/components/Categories.js
@@ -1,115 +1,79 @@
-import React, { Component } from 'react';
-import {
-	ScrollView,
-	Text,
-	StyleSheet,
-	FlatList,
-	TouchableOpacity
-} from 'react-native';
-import {
-	Tile,
-	ImageBackground,
-	Overlay,
-	Icon,
-	Title,
-	Caption,
-	Image,
-	Subtitle,
-	Heading,
-	Row,
-	View
-} from '@shoutem/ui';
-import commonStyles from 'src/common/styles';
+import React, { Component } from "react";
+import { FlatList, TouchableOpacity } from "react-native";
+
+import { Title, Image, View } from "@shoutem/ui";
+import main from "src/common/styles";
 
 export default class Categories extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			posts: [
-				{
-					userId: 1,
-					id: 1,
-					title:
-						'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-					body:
-						'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
-				},
-				{
-					userId: 1,
-					id: 2,
-					title: 'qui est esse',
-					body:
-						'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla'
-				},
-				{
-					userId: 1,
-					id: 3,
-					title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut',
-					body:
-						'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut'
-				},
-				{
-					userId: 1,
-					id: 4,
-					title: 'eum et est occaecati',
-					body:
-						'ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit'
-				}
-			],
-			loading: true
-		};
-	}
+    this.state = {
+      categories: [
+        {
+          userId: 1,
+          id: 1,
+          title:
+            "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+          body:
+            "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: "qui est esse",
+          body:
+            "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+        },
+        {
+          userId: 1,
+          id: 3,
+          title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+          body:
+            "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+        },
+        {
+          userId: 1,
+          id: 4,
+          title: "eum et est occaecati",
+          body:
+            "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+        }
+      ]
+    };
+  }
 
-	_onPressItem = id => {
-		// console.warn('props', this.props);
-		// console.warn('pressed on featured', id);
-	};
+  render() {
+    const { categories } = this.state;
 
-	_keyExtractor = (item, index) => item.id.toString();
+    return (
+      <View>
+        <Title style={main.title}>Categories</Title>
+        <FlatList
+          horizontal
+          data={categories}
+          renderItem={this.renderItem}
+          keyExtractor={this.keyExtractor}
+        />
+      </View>
+    );
+  }
 
-	_renderItem = ({ item }) => {
-		return (
-			<TouchableOpacity onPress={this._onPressItem}>
-				<Image
-					key={item.id}
-					styleName="medium"
-					style={{ marginLeft: 10 }}
-					source={{
-						uri:
-							'https://shoutem.github.io/img/ui-toolkit/examples/image-12.png'
-					}}
-				/>
-			</TouchableOpacity>
-		);
-	};
+  onSelect = category => {
+    console.log("Category selected", category);
+  };
 
-	render() {
-		const { posts } = this.state;
+  keyExtractor = item => item.id.toString();
 
-		return (
-			<View>
-				<Title
-					style={{
-						paddingLeft: 20,
-						paddingVertical: 10,
-						fontWeight: '400'
-					}}>
-					Categories
-				</Title>
-				<FlatList
-					data={posts}
-					keyExtractor={this._keyExtractor}
-					renderItem={this._renderItem}
-					horizontal
-				/>;
-			</View>
-		);
-	}
+  renderItem = () => (
+    <TouchableOpacity onPress={this.onSelect}>
+      <Image
+        styleName="medium"
+        style={{ marginLeft: 10 }}
+        source={{
+          uri: "https://shoutem.github.io/img/ui-toolkit/examples/image-12.png"
+        }}
+      />
+    </TouchableOpacity>
+  );
 }
-
-const styles = StyleSheet.create({
-	termsText: {
-		fontSize: 12
-	}
-});
--- src/Dashboard/components/Featured.js
+++ src/Dashboard/components/Featured.js
@@ -1,66 +1,41 @@
-import React, { Component } from 'react';
-import { FlatList, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { connect } from 'react-redux';
-import {
-	Tile,
-	ImageBackground,
-	Overlay,
-	Icon,
-	Title,
-	Caption,
-	Image,
-	Subtitle,
-	Heading,
-	Row,
-	View
-} from '@shoutem/ui';
-import PostItem from './PostItem';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { FlatList } from "react-native";
+import { connect } from "react-redux";
+
+import { Title, View } from "@shoutem/ui";
+import DealItem from "./DealItem";
 
 class Featured extends Component {
-	constructor(props) {
-		super(props);
+  render() {
+    const { deals } = this.props;
+    return (
+      <View>
+        <FlatList
+          data={deals}
+          renderItem={this.renderItem}
+          keyExtractor={this.keyExtractor}
+          ListHeaderComponent={
+            <Title style={{ paddingLeft: 20, paddingVertical: 10 }}>
+              Featured
+            </Title>
+          }
+        />
+      </View>
+    );
+  }
 
-		this.state = {
-			loading: true
-		};
-	}
+  keyExtractor = item => item.id.toString();
 
-	_keyExtractor = (item, index) => item.id.toString();
-
-	_onPressItem = id => {
-		console.warn('props', this.props);
-		console.warn('pressed on featured', id);
-	};
-
-	_renderItem = ({ item }) => {
-		return (
-			<PostItem
-				id={item.id}
-				item={item}
-				onPressItem={this.props.onOpen}
-				title={item.title}
-			/>
-		);
-	};
-
-	render() {
-		const { posts } = this.props;
-		return (
-			<View>
-				<FlatList
-					data={posts}
-					extraData={this.state}
-					keyExtractor={this._keyExtractor}
-					renderItem={this._renderItem}
-					ListHeaderComponent={
-						<Title style={{ paddingLeft: 20, paddingVertical: 10 }}>
-							Featured
-						</Title>
-					}
-				/>
-			</View>
-		);
-	}
+  renderItem = ({ item }) => (
+    <DealItem item={item} onSelect={this.props.onSelect} />
+  );
 }
+
+Featured.propTypes = {
+  deals: PropTypes.array,
+  onOpen: PropTypes.func,
+  onSelect: PropTypes.func
+};
 
 export default connect(state => state)(Featured);
