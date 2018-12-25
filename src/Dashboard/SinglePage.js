ae83ec7dc56060db99aa017f2913bbe77904b008
ashtrus <zygimantas.butas@gmail.com>
2018-12-25 15:04:00 +0100

    restructured. cleaned up. linted.

--- src/Dashboard/SinglePage.js
+++ src/Dashboard/SinglePage.js
@@ -1,76 +1,71 @@
-import React, { Component } from 'react';
-import { Alert, ScrollView, View, Text, StyleSheet } from 'react-native';
-import {
-	Tile,
-	ImageBackground,
-	Overlay,
-	Title,
-	Caption,
-	Image,
-	Subtitle,
-	Heading,
-	Row
-} from '@shoutem/ui';
-import Button from 'src/common/components/Button';
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import { Alert, ScrollView, View, Text, StyleSheet } from "react-native";
+import { connect } from "react-redux";
 
-export default class SinglePage extends Component {
-	onBook = () => {
-		this.validateBooking();
-	};
+import { Title, Caption, Image, Subtitle, Row } from "@shoutem/ui";
+import Button from "src/common/components/Button";
 
-	validateBooking = () => {
-		Alert.alert(
-			'Book appointment',
-			'Are you sure',
-			[
-				{
-					text: 'Cancel',
-					onPress: () => console.log('Cancel Pressed'),
-					style: 'cancel'
-				},
-				{ text: 'OK', onPress: () => console.log('OK Pressed') }
-			],
-			{ cancelable: false }
-		);
-	};
+class SinglePage extends PureComponent {
+  render() {
+    const { title, body } = this.props;
+    return (
+      <ScrollView>
+        <Image
+          styleName="large-banner"
+          source={{
+            uri: "https://shoutem.github.io/img/ui-toolkit/examples/image-5.png"
+          }}
+        />
 
-	render() {
-		const { title, body } = this.props;
-		// console.warn('this.props', this.props);
-		// const { item } = this.props;
-		return (
-			<ScrollView>
-				<Tile>
-					<Image
-						styleName="large-banner"
-						source={{
-							uri:
-								'https://shoutem.github.io/img/ui-toolkit/examples/image-5.png'
-						}}
-					/>
-					<View styleName="content" style={styles.container}>
-						<Title>{title}</Title>
-						<View styleName="horizontal space-between">
-							<Caption>14d 18:30</Caption>
-							<Caption>100 / 30</Caption>
-						</View>
-					</View>
-				</Tile>
-				<View style={styles.container}>
-					<Text style={styles.container}>{body}</Text>
-					<Text style={styles.container}>{body}</Text>
-					<Subtitle>CVR: 12345678</Subtitle>
+        <View style={{ padding: 10 }}>
+          <Title>{title}</Title>
 
-					<Subtitle> Street 212 3th København 2300</Subtitle>
-					<Button onPress={this.onBook}>Book</Button>
-				</View>
-			</ScrollView>
-		);
-	}
+          <Row>
+            <Caption>14d 18:30</Caption>
+            <Caption>100/30</Caption>
+          </Row>
+
+          <Text>{body}</Text>
+          <Text>{body}</Text>
+
+          <View style={{ paddingVertical: 20 }}>
+            <Subtitle>CVR: 12345678</Subtitle>
+            <Subtitle>Street 212 3th København 2300</Subtitle>
+          </View>
+
+          <Button onPress={this.onBook}>Book</Button>
+        </View>
+      </ScrollView>
+    );
+  }
+
+  onBook = () => {
+    this.validateBooking();
+  };
+
+  validateBooking = () => {
+    Alert.alert(
+      "Book appointment",
+      "Are you sure",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ],
+      { cancelable: false }
+    );
+  };
 }
 
-const styles = StyleSheet.create({
-	container: {
-		padding: 10
-	}
-});
+SinglePage.propTypes = {
+  title: PropTypes.string,
+  body: PropTypes.string
+};
+
+export default connect(state => state)(SinglePage);
+
+const styles = StyleSheet.create({});
