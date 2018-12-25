a72d51e3cd306a73bd91e4c7daf5628f981ff912
ashtrus <zygimantas.butas@gmail.com>
2018-12-25 15:06:42 +0100

    linted

--- src/Map/MapPage.js
+++ src/Map/MapPage.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import { connect } from "react-redux";
 
 class MapPage extends Component {
-	render() {
-		return (
-			<View>
-				<Text> MapPage </Text>
-			</View>
-		);
-	}
+  render() {
+    return (
+      <View>
+        <Text>MapPage</Text>
+      </View>
+    );
+  }
 }
 
 export default connect(state => state)(MapPage);
