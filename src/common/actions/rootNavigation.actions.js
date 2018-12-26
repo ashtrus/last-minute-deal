dc9d72f5164418a29f787663923332e19264de3c
ashtrus <zygimantas.butas@gmail.com>
2018-12-26 20:39:56 +0100

    loading page deleted. roots updated. eslint

--- src/common/actions/rootNavigation.actions.js
+++ src/common/actions/rootNavigation.actions.js
@@ -1,14 +1,13 @@
 export const ROOTS = {
-	LOADING: 'LOADING',
-	AUTH: 'AUTH',
-	MAIN_APP: 'MAIN_APP',
-	MAIN_APP_COMPANY: 'MAIN_APP_COMPANY'
+  AUTH: "AUTH",
+  MAIN_APP: "MAIN_APP",
+  MAIN_APP_COMPANY: "MAIN_APP_COMPANY"
 };
 
 export const types = {
-	CHANGE_ROOT: 'CHANGE_ROOT'
+  CHANGE_ROOT: "CHANGE_ROOT"
 };
 
 export const changeRoot = root => dispatch => {
-	return dispatch({ type: types.CHANGE_ROOT, payload: root });
+  return dispatch({ type: types.CHANGE_ROOT, payload: root });
 };
