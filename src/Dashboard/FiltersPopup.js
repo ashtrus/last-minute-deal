4e7e68f54d18007fc5342b1c92732f8dbc9ea5b7
ashtrus <zygimantas.butas@gmail.com>
2018-12-26 21:45:33 +0100

    component updated. props added. eslint

--- src/Dashboard/FiltersPopup.js
+++ src/Dashboard/FiltersPopup.js
@@ -1,71 +1,58 @@
-import React, { Component } from 'react';
-import { ScrollView, Text } from 'react-native';
-import { connect } from 'react-redux';
-import Categories from './components/Categories';
-import { dismissModal } from 'src/utils/navUtils';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { ScrollView, Slider } from "react-native";
+import { connect } from "react-redux";
+import { dismissModal } from "src/utils/navUtils";
 
-import {
-	Tile,
-	ImageBackground,
-	Overlay,
-	Icon,
-	Title,
-	Caption,
-	Image,
-	Subtitle,
-	Heading,
-	Row,
-	Switch,
-	Divider,
-	View
-} from '@shoutem/ui';
+import { Caption, Divider } from "@shoutem/ui";
 
 class FiltersPopup extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+    this.state = { sliderValue: 10 };
+  }
 
-		this.state = {
-			switchOn: false
-		};
-		this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
-	}
+  onNavigatorEvent = event => {
+    switch (event.id) {
+      case "saveFilters":
+        this.saveFilters();
+      case "closeFilters":
+        dismissModal();
+      default:
+        return;
+    }
+  };
 
-	onNavigatorEvent = event => {
-		switch (event.id) {
-			case 'saveFilters':
-				return console.warn('saveFilters');
-			case 'closeFilters':
-				return dismissModal();
-			default:
-				return;
-		}
-	};
+  saveFilters() {
+    dismissModal();
+  }
 
-	render() {
-		const { switchOn } = this.state;
+  onSliderValueChange = value => {
+    this.setState({ sliderValue: value });
+  };
 
-		return (
-			<ScrollView>
-				<Divider styleName="section-header">
-					<Caption>Distance</Caption>
-					<Caption>km</Caption>
-				</Divider>
-				<Title>Slider here ----------- </Title>
-				<Divider styleName="section-header">
-					<Caption>Appointment time</Caption>
-					<Caption>hh:mm</Caption>
-				</Divider>
-				<Title>Slider here ----------- </Title>
-				<View styleName="space-between horizontal">
-					<Text>Show Sold out</Text>
-					<Switch
-						onValueChange={() => this.setState({ switchOn: !switchOn })}
-						value={switchOn}
-					/>
-				</View>
-			</ScrollView>
-		);
-	}
+  render() {
+    const { sliderValue } = this.state;
+    return (
+      <ScrollView>
+        <Divider styleName="section-header">
+          <Caption>Distance</Caption>
+          <Caption>{sliderValue} km</Caption>
+        </Divider>
+        <Slider
+          minimumValue={0}
+          maximumValue={100}
+          step={1}
+          onValueChange={this.onSliderValueChange}
+        />
+      </ScrollView>
+    );
+  }
 }
+
+FiltersPopup.propTypes = {
+  navigator: PropTypes.func
+};
 
 export default connect(state => state)(FiltersPopup);
