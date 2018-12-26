26e49f4c4311bf0a479c2ba9f7663c0ba80fb09c
ashtrus <zygimantas.butas@gmail.com>
2018-12-26 17:43:14 +0100

    components split. props added. eslint

---
+++ src/Receipts/Receipt.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { TouchableOpacity } from "react-native";
+
+import { Icon, Caption, Image, Subtitle, Row, Button, View } from "@shoutem/ui";
+
+class Receipt extends Component {
+  render() {
+    const {
+      item: { imgUrl, title, discountedPrice, originalPrice }
+    } = this.props;
+
+    return (
+      <TouchableOpacity>
+        <Row>
+          <Image styleName="small rounded-corners" source={{ uri: imgUrl }} />
+          <View styleName="vertical stretch space-between">
+            <Subtitle>{title}</Subtitle>
+            <View styleName="horizontal">
+              <Subtitle styleName="md-gutter-right">
+                ${discountedPrice}
+              </Subtitle>
+              <Caption styleName="line-through">${originalPrice}</Caption>
+            </View>
+          </View>
+          <Button styleName="right-icon">
+            <Icon name="add-to-cart" />
+          </Button>
+        </Row>
+      </TouchableOpacity>
+    );
+  }
+}
+
+Receipt.propTypes = {
+  item: PropTypes.object
+};
+
+export default Receipt;
--- src/Receipts/ReceiptsPage.js
+++ src/Receipts/ReceiptsPage.js
@@ -1,112 +1,87 @@
-import React, { Component } from 'react';
-import { ScrollView, Text } from 'react-native';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { ScrollView, FlatList } from "react-native";
+import { connect } from "react-redux";
 
-import {
-	Tile,
-	ImageBackground,
-	Overlay,
-	Icon,
-	Title,
-	Caption,
-	Image,
-	Subtitle,
-	Heading,
-	Row,
-	Button,
-	View
-} from '@shoutem/ui';
-import commonStyles from 'src/common/styles';
-import { Divider } from 'src/common/components';
+import commonStyles from "src/common/styles";
+import { Divider } from "src/common/components";
+import Receipt from "./Receipt";
 
 class ReceiptsPage extends Component {
-	render() {
-		return (
-			<ScrollView style={commonStyles.container}>
-				<Divider text="Available" />
+  constructor(props) {
+    super(props);
+    this.state = {
+      receipts: [
+        {
+          id: 1,
+          imgUrl:
+            "https://shoutem.github.io/img/ui-toolkit/examples/image-11.png",
+          title: "Family Safari Vacation To The Home Of The Gods",
+          discountedPrice: 120,
+          originalPrice: 150
+        },
+        {
+          id: 2,
+          imgUrl:
+            "https://shoutem.github.io/img/ui-toolkit/examples/image-11.png",
+          title: "Family Safari Vacation To The Home Of The Gods",
+          discountedPrice: 120,
+          originalPrice: 150
+        }
+      ],
+      usedReceipts: [
+        {
+          id: 1,
+          imgUrl:
+            "https://shoutem.github.io/img/ui-toolkit/examples/image-11.png",
+          title: "Family Safari Vacation To The Home Of The Gods",
+          discountedPrice: 120,
+          originalPrice: 150
+        },
+        {
+          id: 2,
+          imgUrl:
+            "https://shoutem.github.io/img/ui-toolkit/examples/image-11.png",
+          title: "Family Safari Vacation To The Home Of The Gods",
+          discountedPrice: 120,
+          originalPrice: 150
+        },
+        {
+          id: 3,
+          imgUrl:
+            "https://shoutem.github.io/img/ui-toolkit/examples/image-11.png",
+          title: "Family Safari Vacation To The Home Of The Gods",
+          discountedPrice: 120,
+          originalPrice: 150
+        }
+      ]
+    };
+  }
 
-				<Row>
-					<Image
-						styleName="small rounded-corners"
-						source={{
-							uri:
-								'https://shoutem.github.io/img/ui-toolkit/examples/image-11.png'
-						}}
-					/>
-					<View styleName="vertical stretch space-between">
-						<Subtitle>Family Safari Vacation To The Home Of The Gods</Subtitle>
-						<View styleName="horizontal">
-							<Subtitle styleName="md-gutter-right">$120.00</Subtitle>
-							<Caption styleName="line-through">$150.00</Caption>
-						</View>
-					</View>
-					<Button styleName="right-icon">
-						<Icon name="add-to-cart" />
-					</Button>
-				</Row>
-				<Row>
-					<Image
-						styleName="small rounded-corners"
-						source={{
-							uri:
-								'https://shoutem.github.io/img/ui-toolkit/examples/image-11.png'
-						}}
-					/>
-					<View styleName="vertical stretch space-between">
-						<Subtitle>Family Safari Vacation To The Home Of The Gods</Subtitle>
-						<View styleName="horizontal">
-							<Subtitle styleName="md-gutter-right">$120.00</Subtitle>
-							<Caption styleName="line-through">$150.00</Caption>
-						</View>
-					</View>
-					<Button styleName="right-icon">
-						<Icon name="add-to-cart" />
-					</Button>
-				</Row>
+  render() {
+    const { receipts, usedReceipts } = this.state;
 
-				<Divider text="Used" />
+    return (
+      <ScrollView style={commonStyles.container}>
+        <Divider text="Available" />
+        <FlatList
+          data={receipts}
+          renderItem={this.renderItem}
+          keyExtractor={this.keyExtractor}
+        />
 
-				<Row>
-					<Image
-						styleName="small rounded-corners"
-						source={{
-							uri:
-								'https://shoutem.github.io/img/ui-toolkit/examples/image-11.png'
-						}}
-					/>
-					<View styleName="vertical stretch space-between">
-						<Subtitle>Family Safari Vacation To The Home Of The Gods</Subtitle>
-						<View styleName="horizontal">
-							<Subtitle styleName="md-gutter-right">$120.00</Subtitle>
-							<Caption styleName="line-through">$150.00</Caption>
-						</View>
-					</View>
-					<Button styleName="right-icon">
-						<Icon name="add-to-cart" />
-					</Button>
-				</Row>
-				<Row>
-					<Image
-						styleName="small rounded-corners"
-						source={{
-							uri:
-								'https://shoutem.github.io/img/ui-toolkit/examples/image-11.png'
-						}}
-					/>
-					<View styleName="vertical stretch space-between">
-						<Subtitle>Family Safari Vacation To The Home Of The Gods</Subtitle>
-						<View styleName="horizontal">
-							<Subtitle styleName="md-gutter-right">$120.00</Subtitle>
-							<Caption styleName="line-through">$150.00</Caption>
-						</View>
-					</View>
-					<Button styleName="right-icon">
-						<Icon name="add-to-cart" />
-					</Button>
-				</Row>
-			</ScrollView>
-		);
-	}
+        <Divider text="Used" />
+        <FlatList
+          data={usedReceipts}
+          renderItem={this.renderItem}
+          keyExtractor={this.keyExtractor}
+        />
+      </ScrollView>
+    );
+  }
+
+  keyExtractor = item => item.id.toString();
+
+  renderItem = ({ item }) => <Receipt item={item} />;
 }
 
 export default connect(state => state)(ReceiptsPage);
