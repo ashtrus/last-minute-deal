0f8e42f1d982a834b5764c367ecdc2bf353ff509
ashtrus <zygimantas.butas@gmail.com>
2018-12-26 21:03:02 +0100

    converted to functional component. props added. eslint

--- src/common/components/Divider.js
+++ src/common/components/Divider.js
@@ -1,24 +1,27 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import colors from 'src/common/colors';
+import React from "react";
+import PropTypes from "prop-types";
+import { Text, View, StyleSheet } from "react-native";
+import colors from "src/common/colors";
 
-export default class Divider extends Component {
-	render() {
-		const { text } = this.props;
-		return (
-			<View style={styles.container}>
-				<Text style={styles.text}>{text}</Text>
-			</View>
-		);
-	}
-}
+const Divider = props => (
+  <View style={styles.container}>
+    <Text style={styles.text}>{props.text}</Text>
+  </View>
+);
+
+export default Divider;
+
+Divider.propTypes = {
+  text: PropTypes.string
+};
+
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: colors.veryLightGray
-	},
-	text: {
-		fontSize: 16,
-		paddingHorizontal: 20,
-		paddingVertical: 10
-	}
+  container: {
+    backgroundColor: colors.lightGray
+  },
+  text: {
+    fontSize: 16,
+    paddingHorizontal: 20,
+    paddingVertical: 10
+  }
 });
