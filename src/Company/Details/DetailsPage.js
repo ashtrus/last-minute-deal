fdb5f90c21594ba32f489f42a13955e07b5c3ecc
ashtrus <zygimantas.butas@gmail.com>
2018-12-29 23:28:58 +0100

    form added to company details page

--- src/Company/Details/DetailsPage.js
+++ src/Company/Details/DetailsPage.js
@@ -1,15 +1,44 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { connect } from "react-redux";
+
+import { Form, Item, Input, Label } from "native-base";
 
 class CompanyDetailsPage extends Component {
   render() {
     return (
       <View>
-        <Text>CompanyDetailsPage</Text>
+        <Form>
+          <Item floatingLabel>
+            <Label>Company name</Label>
+            <Input onChangeText={this.onChange} />
+          </Item>
+
+          <Item floatingLabel last>
+            <Label>Address</Label>
+            <Input onChangeText={this.onChange} />
+          </Item>
+
+          <Item floatingLabel last>
+            <Label>Phone</Label>
+            <Input keyboardType={"numeric"} onChangeText={this.onChange} />
+          </Item>
+
+          <Item floatingLabel last>
+            <Label>Email</Label>
+            <Input
+              keyboardType={"email-address"}
+              onChangeText={this.onChange}
+            />
+          </Item>
+        </Form>
       </View>
     );
   }
+
+  onChange = data => {
+    console.log("### data changed", data);
+  };
 }
 
 export default connect(state => state)(CompanyDetailsPage);
