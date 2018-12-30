7d0d2ef1fb2262ab4db7b6a3a5e2b5e11beb1f14
ashtrus <zygimantas.butas@gmail.com>
2018-12-30 20:44:07 +0100

    Replaced with native-base Divider

--- src/common/components/Divider.js
+++ src/common/components/Divider.js
@@ -1,12 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Text, View, StyleSheet } from "react-native";
-import colors from "src/common/colors";
+import { Text } from "react-native";
+import { ListItem } from "native-base";
 
 const Divider = props => (
-  <View style={styles.container}>
-    <Text style={styles.text}>{props.text}</Text>
-  </View>
+  <ListItem itemDivider>
+    <Text>{props.text}</Text>
+  </ListItem>
 );
 
 export default Divider;
@@ -14,14 +14,3 @@ export default Divider;
 Divider.propTypes = {
   text: PropTypes.string
 };
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: colors.lightGray
-  },
-  text: {
-    fontSize: 16,
-    paddingHorizontal: 20,
-    paddingVertical: 10
-  }
-});
