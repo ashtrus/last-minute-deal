a01cb070ad4cbbe7fe48b2ee337c3ad6038d83b7
ashtrus <zygimantas.butas@gmail.com>
2018-12-31 17:44:11 +0100

    read deals from props. edit and delete functions added.

--- src/Company/Dashboard/DashboardPage.js
+++ src/Company/Dashboard/DashboardPage.js
@@ -8,71 +8,13 @@ import { Spinner } from "native-base";
 import Deals from "./components/Deals";
 import { showModal } from "src/utils/navUtils";
 import main from "src/common/styles";
+import { openDealPopup, deleteDeal } from "src/common/actions/deals.actions";
 
 class DashboardPage extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      deals: [
-        {
-          userId: 1,
-          id: 1,
-          title:
-            "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-          body:
-            "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-        },
-        {
-          userId: 1,
-          id: 2,
-          title: "qui est esse",
-          body:
-            "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-        },
-        {
-          userId: 1,
-          id: 3,
-          title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-          body:
-            "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-        },
-        {
-          userId: 1,
-          id: 4,
-          title: "eum et est occaecati",
-          body:
-            "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
-        },
-        {
-          userId: 1,
-          id: 5,
-          title: "nesciunt quas odio",
-          body:
-            "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
-        },
-        {
-          userId: 1,
-          id: 6,
-          title: "dolorem eum magni eos aperiam quia",
-          body:
-            "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
-        },
-        {
-          userId: 1,
-          id: 7,
-          title: "magnam facilis autem",
-          body:
-            "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas"
-        },
-        {
-          userId: 1,
-          id: 8,
-          title: "dolorem dolore est ipsam",
-          body:
-            "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae"
-        }
-      ],
       // TODO: set to true when real data used
       loading: false
     };
@@ -90,7 +32,10 @@ class DashboardPage extends Component {
   };
 
   render() {
-    const { loading, deals } = this.state;
+    const { loading } = this.state;
+    const {
+      deals: { items }
+    } = this.props;
 
     return Boolean(loading) ? (
       <View style={main.containerCenter}>
@@ -98,7 +43,12 @@ class DashboardPage extends Component {
       </View>
     ) : (
       <ScrollView>
-        <Deals deals={deals} onSelect={this.openDetailsPage} />
+        <Deals
+          deals={items}
+          onSelect={this.openDetailsPage}
+          onEdit={id => this.onEdit(id)}
+          onDelete={id => this.onDelete(id)}
+        />
       </ScrollView>
     );
   }
@@ -142,10 +92,16 @@ class DashboardPage extends Component {
       }
     });
   };
+
+  onEdit = id => this.props.dispatch(openDealPopup({ id }));
+
+  onDelete = id => this.props.dispatch(deleteDeal(id));
 }
 
 DashboardPage.propTypes = {
-  navigator: PropTypes.object
+  navigator: PropTypes.object,
+  dispatch: PropTypes.func,
+  deals: PropTypes.object
 };
 
 export default connect(state => state)(DashboardPage);
