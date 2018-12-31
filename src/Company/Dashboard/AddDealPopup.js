28b026cc703ebe8a227d7f0234af2f2d63edca91
ashtrus <zygimantas.butas@gmail.com>
2018-12-31 17:45:29 +0100

    add deal form added. save deal to redux

--- src/Company/Dashboard/AddDealPopup.js
+++ src/Company/Dashboard/AddDealPopup.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { connect } from "react-redux";
-import { dismissModal } from "src/utils/navUtils";
 
+import { Form, Input, Item, Label } from "native-base";
+
+import { dismissModal } from "src/utils/navUtils";
+import { createDeal } from "src/common/actions/deals.actions";
 class AddDealPopup extends Component {
   constructor(props) {
     super(props);
-    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+    this.state = {
+      deal: props.deal || {}
+    };
+    props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
   }
 
   onNavigatorEvent = event => {
@@ -22,20 +28,75 @@ class AddDealPopup extends Component {
   };
 
   render() {
+    const {
+      deal: { title, imgUrl, discountedPrice, originalPrice }
+    } = this.state;
+
     return (
-      <View styleName="space-between horizontal">
-        <Text>Add new deal form here</Text>
+      <View>
+        <Form>
+          <Item floatingLabel>
+            <Label>Title</Label>
+            <Input value={title} onChangeText={this.onChangeTitle} />
+          </Item>
+
+          <Item floatingLabel>
+            <Label>Image url</Label>
+            <Input value={imgUrl} onChangeText={this.onChangeImage} />
+          </Item>
+
+          <Item floatingLabel>
+            <Label>Discounted price</Label>
+            <Input
+              value={discountedPrice}
+              keyboardType={"numeric"}
+              onChangeText={this.onChangeDiscountedPrice}
+            />
+          </Item>
+
+          <Item floatingLabel>
+            <Label>Original price</Label>
+            <Input
+              value={originalPrice}
+              keyboardType={"numeric"}
+              onChangeText={this.onChangeOriginalPrice}
+            />
+          </Item>
+        </Form>
       </View>
     );
   }
 
-  save() {
+  onChangeTitle = title =>
+    this.setState({
+      deal: { ...this.state.deal, title }
+    });
+
+  onChangeAddress = imgUrl =>
+    this.setState({
+      deal: { ...this.state.deal, imgUrl }
+    });
+
+  onChangeDiscountedPrice = discountedPrice =>
+    this.setState({
+      deal: { ...this.state.deal, discountedPrice }
+    });
+
+  onChangeOriginalPrice = originalPrice =>
+    this.setState({
+      deal: { ...this.state.deal, originalPrice }
+    });
+
+  save = () => {
+    this.props.dispatch(createDeal(this.state.deal));
     dismissModal();
-  }
+  };
 }
 
 AddDealPopup.propTypes = {
-  navigator: PropTypes.func
+  navigator: PropTypes.func,
+  dispatch: PropTypes.func,
+  deal: PropTypes.obj
 };
 
 export default connect(state => state)(AddDealPopup);
