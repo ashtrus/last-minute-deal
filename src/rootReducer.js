4c20bbba8107a194895eceea2c3a1588a23814f9
ashtrus <zygimantas.butas@gmail.com>
2019-01-07 21:56:55 +0100

    receipt reducers added

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import auth from "./auth/auth.reducer";
 import currentDeal from "src/common/reducers/currentDeal.reducer";
+import currentReceipt from "src/common/reducers/currentReceipt.reducer";
 import deals from "src/common/reducers/deals.reducer";
 import navigation from "src/common/reducers/navigation.reducer";
+import receipts from "src/common/reducers/receipts.reducer";
 import rootNavigation from "src/common/reducers/rootNavigation.reducer";
 import settings from "src/common/reducers/settings.reducer";
 import userSettings from "src/common/reducers/userSettings.reducer";
@@ -11,8 +13,10 @@ import userSettings from "src/common/reducers/userSettings.reducer";
 const combinedReducers = combineReducers({
   auth,
   currentDeal,
+  currentReceipt,
   deals,
   navigation,
+  receipts,
   rootNavigation,
   settings,
   userSettings
