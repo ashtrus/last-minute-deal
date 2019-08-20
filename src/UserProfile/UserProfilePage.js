f9c9c8974c88fe61a079fcddc9bafb0ae55120eb
ashtrus <zygimantas.butas@gmail.com>
2019-08-20 19:21:40 +0200

    navigation updates

--- src/UserProfile/UserProfilePage.js
+++ src/UserProfile/UserProfilePage.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 import { connect } from "react-redux";
+import { Navigation } from "react-native-navigation";
 
 class UserProfilePage extends Component {
+  constructor(props) {
+    super(props);
+    Navigation.events().bindComponent(this);
+  }
+
   render() {
     return (
       <View>
         <Text> UserProfilePage </Text>
       </View>
     );
+  }
+
+  navigationButtonPressed({ buttonId }) {
+    if (buttonId === "backButton") {
+      Navigation.pop(this.props.componentId);
+    }
   }
 }
 
