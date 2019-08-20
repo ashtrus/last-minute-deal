d65e445b3d0205b2c186ae194bd4356be9a80e69
ashtrus <zygimantas.butas@gmail.com>
2019-08-20 19:47:23 +0200

    refactored

--- src/utils/theme/icons.js
+++ src/utils/theme/icons.js
@@ -2,7 +2,7 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 import SimpleLineIcons from "react-native-vector-icons/SimpleLineIcons";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { PixelRatio } from "react-native";
-import { isAndroid } from "../../common/styles";
+import { isAndroid } from "src/common/styles";
 
 const navIconSize = isAndroid() ? PixelRatio.getPixelSizeForLayoutSize(25) : 25;
 
@@ -25,17 +25,18 @@ const icons = {
 
 const iconsMap = {};
 
-const iconsLoaded = new Promise((resolve, reject) => {
-  // eslint-disable-next-line
-  new Promise.all(
-    Object.keys(icons).map(iconName => {
-      const Provider = icons[iconName][1];
-      return Provider.getImageSource(iconName.replace(replaceSuffixPattern, ""), icons[iconName][0]);
-    })
-  ).then(sources => {
-    Object.keys(icons).forEach((iconName, i) => (iconsMap[iconName] = sources[i]));
-    resolve(true);
-  });
-});
+const iconsLoaded = async () => {
+  try {
+    const sources = await Promise.all(
+      Object.keys(icons).map(iconName => {
+        const Provider = icons[iconName][1];
+        return Provider.getImageSource(iconName.replace(replaceSuffixPattern, ""), icons[iconName][0]);
+      })
+    );
+    await Object.keys(icons).forEach((iconName, i) => (iconsMap[iconName] = sources[i]));
+  } catch (error) {
+    console.warn("ERROR", error);
+  }
+};
 
 export { iconsMap, iconsLoaded };
