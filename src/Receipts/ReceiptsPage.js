7463291eebc9fa87fd82b1662277b3fcb6d8cf9d
ashtrus <zygimantas.butas@gmail.com>
2019-08-25 22:18:58 +0200

    refactored, constructor removed. props instead state

--- src/Receipts/ReceiptsPage.js
+++ src/Receipts/ReceiptsPage.js
@@ -1,52 +1,41 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { ScrollView, FlatList } from "react-native";
 import { connect } from "react-redux";
-import { loadReceipts } from "src/common/actions/receipts.actions";
-
-import commonStyles from "src/common/styles";
+import { ScrollView, FlatList } from "react-native";
+import { Container, Content } from "native-base";
 import { Divider } from "src/common/components";
 import Receipt from "./Receipt";
+import commonStyles from "src/common/styles";
+import { loadReceipts } from "src/common/actions/receipts.actions";
 
 class ReceiptsPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      usedReceipts: props.receipts.items.slice(2, 7)
-    };
-  }
-
   componentDidMount() {
     try {
       this.props.dispatch(loadReceipts());
     } catch (error) {
-      console.error("Get deals failed.", error);
+      console.error("Get receipts failed.", error);
     }
   }
 
   render() {
-    const { usedReceipts } = this.state;
     const {
       receipts: { items }
     } = this.props;
+    const availableReceipts = items.slice(0, 2);
+    const usedReceipts = items.slice(2, 10);
 
     return (
-      // prettier-ignore
-      <ScrollView style={commonStyles.container}>
-        <Divider text="Available" />
-        <FlatList
-          data={items.slice(0,2)}
-          renderItem={this.renderItem}
-          keyExtractor={this.keyExtractor}
-        />
+      <Container>
+        <ScrollView style={commonStyles.container}>
+          <Content padder>
+            <Divider text="Available" />
+            <FlatList data={availableReceipts} renderItem={this.renderItem} keyExtractor={this.keyExtractor} />
 
-        <Divider text="Used" />
-        <FlatList
-          data={usedReceipts}
-          renderItem={this.renderItem}
-          keyExtractor={this.keyExtractor}
-        />
-      </ScrollView>
+            <Divider text="Used" />
+            <FlatList data={usedReceipts} renderItem={this.renderItem} keyExtractor={this.keyExtractor} />
+          </Content>
+        </ScrollView>
+      </Container>
     );
   }
 
