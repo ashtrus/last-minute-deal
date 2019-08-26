d7c27eeb202ecf5870349a9225aeb8c09282e008
ashtrus <zygimantas.butas@gmail.com>
2019-08-26 21:37:14 +0200

    refactored. converted to functional component

--- src/Company/Dashboard/components/Deals.js
+++ src/Company/Dashboard/components/Deals.js
@@ -1,38 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { FlatList } from "react-native";
-import { connect } from "react-redux";
-
-import { View } from "@shoutem/ui";
 import DealItem from "./DealItem";
 
-class Deals extends Component {
-  render() {
-    const {
-      deals: { items }
-    } = this.props;
-
-    // prettier-ignore
-    return (
-      <View>
-        <FlatList
-          data={items.slice(0, 10)}
-          renderItem={this.renderItem}
-          keyExtractor={this.keyExtractor}
-        />
-      </View>
-    );
-  }
-
-  keyExtractor = item => item.id.toString();
-
-  renderItem = ({ item }) => <DealItem item={item} onSelect={this.props.onSelect} />;
-}
+const Deals = ({ deals, componentId }) => (
+  <FlatList
+    data={deals.slice(0, 10)}
+    renderItem={({ item }) => <DealItem item={item} componentId={componentId} />}
+    keyExtractor={item => item.id.toString()}
+  />
+);
 
 Deals.propTypes = {
   deals: PropTypes.object,
-  onOpen: PropTypes.func,
-  onSelect: PropTypes.func
+  componentId: PropTypes.string
 };
 
-export default connect(state => state)(Deals);
+export default Deals;
