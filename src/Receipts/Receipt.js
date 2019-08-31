0dd2678a2bb9fa61e1eb6e5bda19e7d7018bf466
ashtrus <zygimantas.butas@gmail.com>
2019-08-31 20:23:44 +0200

    Updates: - Transformed to functional component - Native Base components used

--- src/Receipts/Receipt.js
+++ src/Receipts/Receipt.js
@@ -1,36 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { TouchableOpacity } from "react-native";
-
-import { Icon, Caption, Image, Subtitle, Row, Button, View } from "@shoutem/ui";
+import { Image } from "react-native";
+import { Card, CardItem, Text, Body } from "native-base";
+import main from "src/common/styles";
 
 const DEFAULT_IMG = require("../../assets/img/spa-logo.png");
 
-class Receipt extends Component {
-  render() {
-    const {
-      item: { imgUrl, title, discountedPrice = 100, originalPrice = 500 }
-    } = this.props;
-
-    return (
-      <TouchableOpacity onPress={() => console.log("### item pressed")}>
-        <Row>
-          <Image styleName="small rounded-corners" source={Boolean(imgUrl) ? { uri: imgUrl } : DEFAULT_IMG} />
-          <View styleName="vertical stretch space-between">
-            <Subtitle>{title}</Subtitle>
-            <View styleName="horizontal">
-              <Subtitle styleName="md-gutter-right">${discountedPrice}</Subtitle>
-              <Caption styleName="line-through">${originalPrice}</Caption>
-            </View>
-          </View>
-          <Button styleName="right-icon">
-            <Icon name="add-to-cart" />
-          </Button>
-        </Row>
-      </TouchableOpacity>
-    );
-  }
-}
+const Receipt = ({ item: { title, body, imgUrl, discountedPrice = 100, originalPrice = 500 } }) => (
+  <Card>
+    <CardItem cardBody button onPress={() => {}}>
+      <Image source={Boolean(imgUrl) ? { uri: imgUrl } : DEFAULT_IMG} style={main.banner} />
+    </CardItem>
+    <CardItem>
+      <Body>
+        <Text style={main.subtitle}>{title}</Text>
+        <Text style={main.caption}>
+          ${discountedPrice} / ${originalPrice}
+        </Text>
+      </Body>
+    </CardItem>
+  </Card>
+);
 
 Receipt.propTypes = {
   item: PropTypes.object
