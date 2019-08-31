074320b30bdc666eeb8164277ea81db011299d5b
ashtrus <zygimantas.butas@gmail.com>
2019-08-31 20:13:00 +0200

    Updates: - converted to functional component - connect removed - Native Base component used - Navigation to details logic moved

--- src/Company/Dashboard/components/DealItem.js
+++ src/Company/Dashboard/components/DealItem.js
@@ -1,54 +1,79 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { TouchableOpacity } from "react-native";
-import { Caption, Subtitle, Row, Image, View } from "@shoutem/ui";
+import { Image } from "react-native";
+import { List, ListItem, Left, Body, Text } from "native-base";
+import main from "src/common/styles";
+import { Navigation } from "react-native-navigation";
 
 const DEFAULT_IMG = require("../../../../assets/img/massage.jpg");
 
-class DealItem extends PureComponent {
-  onPress = () => {
-    const { onSelect, item } = this.props;
-    onSelect(item);
-  };
+const DealItem = ({ item, componentId }) => {
+  const {
+    imgUrl,
+    title = "Service name",
+    companyName = "Branch name / Employee",
+    discountedPrice = 100,
+    originalPrice = 500,
+    time = "14:00 - 15:00",
+    address = "Branch address"
+  } = item;
 
-  render() {
-    const {
-      item: {
-        imgUrl,
-        title = "Service name",
-        companyName = "Branch name / Employee",
-        discountedPrice = 100,
-        originalPrice = 500,
-        time = "14:00 - 15:00",
-        address = "Branch address"
-      }
-    } = this.props;
-
-    return (
-      <TouchableOpacity onPress={this.onPress}>
-        <Row>
-          <Image styleName="medium rounded-corners" source={Boolean(imgUrl) ? { uri: imgUrl } : DEFAULT_IMG} />
-          <View styleName="vertical stretch">
-            <Subtitle>{title}</Subtitle>
-            <Caption>{companyName}</Caption>
-            <Caption>{address}</Caption>
-            <View styleName="horizontal space-between">
-              <Caption>
-                ${discountedPrice} / ${originalPrice}
-              </Caption>
-              <Caption>{time}</Caption>
-            </View>
-          </View>
-        </Row>
-      </TouchableOpacity>
-    );
-  }
-}
+  return (
+    <List>
+      <ListItem
+        thumbnail
+        button
+        onPress={() => openDetailsPage(item, componentId)}
+        style={{ flex: 1, justifyContent: "flex-start", alignSelf: "flex-start" }}
+      >
+        <Left>
+          <Image
+            square
+            source={Boolean(imgUrl) ? { uri: imgUrl } : DEFAULT_IMG}
+            style={[main.thumbnail, { marginLeft: -20 }]}
+          />
+        </Left>
+        <Body>
+          <Text style={main.subtitle}>{title}</Text>
+          <Text style={main.caption}>{address}</Text>
+          <Text style={main.caption}>{companyName}</Text>
+          <Text style={main.caption}>{time}</Text>
+          <Text style={main.caption}>
+            ${discountedPrice} / ${originalPrice}
+          </Text>
+        </Body>
+      </ListItem>
+    </List>
+  );
+};
 
 DealItem.propTypes = {
   item: PropTypes.object,
-  onSelect: PropTypes.func
+  componentId: PropTypes.string
 };
 
-export default connect(state => state)(DealItem);
+export default DealItem;
+
+const openDetailsPage = (deal, componentId) => {
+  Navigation.push(componentId, {
+    component: {
+      name: "LastMinuteDeal.CompanySinglePage",
+      passProps: {
+        deal
+      },
+      options: {
+        topBar: {
+          title: {
+            text: "Deal details"
+          },
+          leftButtons: [
+            {
+              text: "Back",
+              id: "backButton"
+            }
+          ]
+        }
+      }
+    }
+  });
+};
